82bc4f4f0a5ee9722db7c7fd36231707860fcad8
Joksa132 <nikolajoksimovic419@gmail.com>
2023-09-23 20:40:19 +0200

    fix padding and height

--- components/MarkdownInput.tsx
+++ components/MarkdownInput.tsx
@@ -2,10 +2,10 @@ export default function MarkdownInput() {
 
   return (
     <div className="w-1/2 border-[1px] border-slate-200 flex flex-col">
-      <div className="p-3">
+      <div className="px-4 py-3">
         <h2 className="uppercase text-xs text-slate-400 tracking-wider">markdown</h2>
       </div>
-      <textarea className="resize-none outline-none border-t-[1px] p-2 text-lg h-[calc(100vh-93px)] overflow-y-scroll" />
+      <textarea className="resize-none outline-none border-t-[1px] p-2 text-lg h-[calc(100vh-88px)] overflow-y-scroll" />
     </div>
   )
 }
--- components/MarkdownPreview.tsx
+++ components/MarkdownPreview.tsx
@@ -5,7 +5,7 @@ export default function MarkdownPreview() {
       <div className="px-4 py-3">
         <h2 className="uppercase text-xs text-slate-400 tracking-wider">preview</h2>
       </div>
-      <div className="border-t-[1px] h-[calc(100vh-93px)] overflow-y-scroll"></div>
+      <div className="border-t-[1px] h-[calc(100vh-88px)] overflow-y-scroll"></div>
     </div>
   )
 }
