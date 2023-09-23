9c3788b3f1150741224960e29d69bba29c07c612
Joksa132 <nikolajoksimovic419@gmail.com>
2023-09-23 20:39:57 +0200

    add github link, export as menu and icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,20 @@
+import Link from 'next/link'
+import { BiSolidChevronDown, BiLogoGithub } from 'react-icons/bi'
+
 export default function Header() {
 
   return (
-    <header className="p-4 bg-slate-700 text-white">
-      <h1 className="tracking-widest select-none text-[13px] uppercase hover:text-slate-300">Markdown Playground</h1>
+    <header className="py-2 px-4 bg-slate-700 text-slate-200 flex justify-between items-center uppercase">
+      <h1 className="tracking-widest select-none text-[13px] hover:text-slate-400">Markdown Playground</h1>
+      <div className="flex items-center gap-8">
+        <span className="tracking-widest flex gap-2 items-center text-[11px] cursor-pointer hover:text-slate-400">
+          Export as
+          <BiSolidChevronDown />
+        </span>
+        <Link href={"https://github.com/Joksa132/markdown-playground"} target="_blank">
+          <BiLogoGithub size={30} />
+        </Link>
+      </div>
     </header>
   )
 }
