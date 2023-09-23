01adc00a45904ccf3f5e84f11c95a93b2c53c0a4
Joksa132 <nikolajoksimovic419@gmail.com>
2023-09-23 20:11:36 +0200

    create markdown preview component

---
+++ components/MarkdownPreview.tsx
@@ -0,0 +1,11 @@
+export default function MarkdownPreview() {
+
+  return (
+    <div className="w-1/2 border-[1px] border-slate-200 flex flex-col">
+      <div className="px-4 py-3">
+        <h2 className="uppercase text-xs text-slate-400 tracking-wider">preview</h2>
+      </div>
+      <div className="border-t-[1px] h-[calc(100vh-93px)] overflow-y-scroll"></div>
+    </div>
+  )
+}
