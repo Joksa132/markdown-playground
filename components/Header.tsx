75def38bfeba45306e79eaae4bb772cffa88d579
Joksa132 <nikolajoksimovic419@gmail.com>
2023-09-23 20:06:50 +0200

    create basic header

---
+++ components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+
+  return (
+    <header className="p-4 bg-slate-700 text-white">
+      <h1 className="tracking-widest select-none text-[13px] uppercase hover:text-slate-300">Markdown Playground</h1>
+    </header>
+  )
+}
