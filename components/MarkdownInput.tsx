11874c5a7f2c03600280a4b8f5f28a4f1caabbfa
Joksa132 <nikolajoksimovic419@gmail.com>
2023-09-24 16:39:39 +0200

    add setstate on the textarea onchange event

--- components/MarkdownInput.tsx
+++ components/MarkdownInput.tsx
@@ -1,11 +1,20 @@
-export default function MarkdownInput() {
+type Props = {
+  setMarkdownInput: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function MarkdownInput({ setMarkdownInput }: Props) {
 
   return (
     <div className="w-1/2 border-[1px] border-slate-200 flex flex-col">
       <div className="px-4 py-3">
         <h2 className="uppercase text-xs text-slate-400 tracking-wider">markdown</h2>
       </div>
-      <textarea className="resize-none outline-none border-t-[1px] p-2 text-lg h-[calc(100vh-88px)] overflow-y-scroll" />
+      <textarea
+        className="resize-none outline-none border-t-[1px] p-2 text-lg h-[calc(100vh-88px)] overflow-y-scroll"
+        name="markdownInput"
+        id="markdownInput"
+        onChange={(e) => setMarkdownInput(e.target.value)}
+      />
     </div>
   )
 }
