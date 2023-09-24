9de0e3c146b2fa986e25f1174281846e78645994
Joksa132 <nikolajoksimovic419@gmail.com>
2023-09-24 16:40:03 +0200

    add react markdown component

--- components/MarkdownPreview.tsx
+++ components/MarkdownPreview.tsx
@@ -1,11 +1,20 @@
-export default function MarkdownPreview() {
+import ReactMarkdown from "react-markdown";
+import remarkGfm from 'remark-gfm'
+
+type Props = {
+  markdown: string;
+}
+
+export default function MarkdownPreview({ markdown }: Props) {
 
   return (
     <div className="w-1/2 border-[1px] border-slate-200 flex flex-col">
       <div className="px-4 py-3">
         <h2 className="uppercase text-xs text-slate-400 tracking-wider">preview</h2>
       </div>
-      <div className="border-t-[1px] h-[calc(100vh-88px)] overflow-y-scroll"></div>
+      <div className="border-t-[1px] h-[calc(100vh-88px)] overflow-y-scroll p-2">
+        <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} className="prose" />
+      </div>
     </div>
   )
 }
