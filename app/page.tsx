01996375fab26eafa830cba845de85b4ca1461d2
Joksa132 <nikolajoksimovic419@gmail.com>
2023-09-24 16:39:04 +0200

    add state for input and add props to components

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
+"use client"
+
 import Header from "@/components/Header"
 import MarkdownInput from "@/components/MarkdownInput"
 import MarkdownPreview from "@/components/MarkdownPreview"
+import { useState } from "react"
 
 export default function Home() {
+  const [markdownInput, setMarkdownInput] = useState<string>("")
+
   return (
     <div className="flex flex-col w-full h-screen">
       <Header />
       <main className="flex">
-        <MarkdownInput />
-        <MarkdownPreview />
+        <MarkdownInput setMarkdownInput={setMarkdownInput} />
+        <MarkdownPreview markdown={markdownInput} />
       </main>
     </div>
   )
