54ac7f4a4a5763be4e8a27be21f055592d72ff19
Joksa132 <nikolajoksimovic419@gmail.com>
2023-09-25 13:12:40 +0200

    add textarea for help page

--- components/MarkdownInput.tsx
+++ components/MarkdownInput.tsx
@@ -4,9 +4,10 @@ import { EditorView } from "@codemirror/view";
 type Props = {
   markdown: string;
   setMarkdownInput: React.Dispatch<React.SetStateAction<string>>;
+  isMain: boolean;
 }
 
-export default function MarkdownInput({ markdown, setMarkdownInput }: Props) {
+export default function MarkdownInput({ markdown, setMarkdownInput, isMain }: Props) {
 
   const handleMarkdownChange = (markdown: string) => {
     setMarkdownInput(markdown)
@@ -17,16 +18,27 @@ export default function MarkdownInput({ markdown, setMarkdownInput }: Props) {
       <div className="px-4 py-3">
         <h2 className="uppercase text-xs text-slate-400 tracking-wider">markdown</h2>
       </div>
-      <div className="border-t-[1px] overflow-y-scroll h-[calc(100vh-88px)] py-1">
-        <CodeMirror
-          className="text-lg"
-          onChange={handleMarkdownChange}
-          extensions={[
-            EditorView.lineWrapping
-          ]}
-          value={markdown}
-        />
-      </div>
+      {
+        isMain ?
+          <div className="border-t-[1px] overflow-y-scroll h-[calc(100vh-88px)] py-1">
+            <CodeMirror
+              className="text-lg"
+              onChange={handleMarkdownChange}
+              extensions={[
+                EditorView.lineWrapping
+              ]}
+              value={markdown}
+            />
+          </div>
+          :
+          <textarea
+            className="resize-none outline-none border-t-[1px] p-2 text-lg h-[calc(100vh-88px)] overflow-y-scroll"
+            name="markdownHelp"
+            id="markdownHelp"
+            value={markdown}
+            disabled
+          />
+      }
     </div>
   )
 }
