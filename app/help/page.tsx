0b59df0fa2fc9adce34b38ca12e9a04dd018751b
Joksa132 <nikolajoksimovic419@gmail.com>
2023-09-25 14:14:10 +0200

    make help page responsive

--- app/help/page.tsx
+++ app/help/page.tsx
@@ -1,16 +1,30 @@
+"use client"
+
 import Header from "@/components/Header";
 import MarkdownInput from "@/components/MarkdownInput";
 import MarkdownPreview from "@/components/MarkdownPreview";
 import { helpMarkdown } from "@/util/helpMarkdown";
+import { useState } from "react";
 
 export default function Help() {
+  const [showPreview, setShowPreview] = useState<boolean>(false)
 
   return (
     <div className="flex flex-col w-full h-screen">
       <Header />
       <div className="flex">
-        <MarkdownInput markdown={helpMarkdown} setMarkdownInput={() => { }} isMain={false} />
-        <MarkdownPreview markdown={helpMarkdown} />
+        <MarkdownInput
+          markdown={helpMarkdown}
+          setMarkdownInput={() => { }}
+          isMain={false}
+          setShowPreview={setShowPreview}
+          showPreview={showPreview}
+        />
+        <MarkdownPreview
+          markdown={helpMarkdown}
+          setShowPreview={setShowPreview}
+          showPreview={showPreview}
+        />
       </div>
     </div>
   )
