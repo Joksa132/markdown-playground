58c3f7d0e935d1fe66d8da730fec8d7bb8bc4f00
Joksa132 <nikolajoksimovic419@gmail.com>
2023-09-25 11:08:54 +0200

    replace textarea with codemirror

--- components/MarkdownInput.tsx
+++ components/MarkdownInput.tsx
@@ -1,20 +1,30 @@
+import CodeMirror from "@uiw/react-codemirror";
+import { EditorView } from "@codemirror/view";
+
 type Props = {
   setMarkdownInput: React.Dispatch<React.SetStateAction<string>>;
 }
 
 export default function MarkdownInput({ setMarkdownInput }: Props) {
 
+  const handleMarkdownChange = (markdown: string) => {
+    setMarkdownInput(markdown)
+  }
+
   return (
     <div className="w-1/2 border-[1px] border-slate-200 flex flex-col">
       <div className="px-4 py-3">
         <h2 className="uppercase text-xs text-slate-400 tracking-wider">markdown</h2>
       </div>
-      <textarea
-        className="resize-none outline-none border-t-[1px] p-2 text-lg h-[calc(100vh-88px)] overflow-y-scroll"
-        name="markdownInput"
-        id="markdownInput"
-        onChange={(e) => setMarkdownInput(e.target.value)}
-      />
+      <div className="overflow-y-scroll h-[calc(100vh-88px)] py-1">
+        <CodeMirror
+          className="border-t-[1px] text-lg"
+          onChange={handleMarkdownChange}
+          extensions={[
+            EditorView.lineWrapping
+          ]}
+        />
+      </div>
     </div>
   )
 }
