7967a12cbf8d0218fa50d098212ccad9d3805a61
Joksa132 <nikolajoksimovic419@gmail.com>
2023-09-25 14:33:27 +0200

    made header responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,24 +12,25 @@ export default function Header() {
   return (
     <header className="py-2 px-4 bg-slate-700 text-slate-200 flex justify-between items-center uppercase">
       <Link href={"/"}>
-        <h1 className="tracking-widest select-none text-[13px] hover:text-slate-400 cursor-pointer">Markdown Playground</h1>
+        <h1 className="max-[380px]:hidden tracking-widest select-none text-[13px] hover:text-slate-400 cursor-pointer max-[500px]:text-[10px]">Markdown Playground</h1>
+        <h1 className="hidden max-[380px]:block tracking-widest select-none text-[13px] hover:text-slate-400 cursor-pointer max-[500px]:text-[10px]">Home</h1>
       </Link>
-      <div className="flex items-center gap-8">
+      <div className="flex items-center gap-8 text-[11px] max-[600px]:gap-4 max-[500px]:text-[9px] max-[500px]:gap-2">
         <span
-          className="tracking-widest flex gap-2 items-center text-[11px] cursor-pointer hover:text-slate-400"
+          className="tracking-widest flex gap-2 items-center cursor-pointer hover:text-slate-400"
           onClick={() => saveMarkdown(markdownInput)}
         >
           Save
         </span>
         <div className="relative">
           <span
-            className={`tracking-widest flex gap-2 items-center text-[11px] cursor-pointer hover:text-slate-400`}
+            className={`tracking-widest flex gap-2 items-center cursor-pointer hover:text-slate-400`}
             onClick={() => setIsMenuClicked(!isMenuClicked)}
           >
             Export as
             <BiSolidChevronDown />
           </span>
-          <div className={`${isMenuClicked ? 'flex' : 'hidden'} flex-col text-[11px] p-2 gap-2 absolute h-20 w-full bg-slate-800 mt-3`}>
+          <div className={`${isMenuClicked ? 'flex' : 'hidden'} flex-col p-2 gap-2 absolute h-20 w-full bg-slate-800 mt-3 z-20`}>
             <span className="cursor-pointer hover:text-slate-300 tracking-widest">
               Markdown
             </span>
@@ -42,7 +43,7 @@ export default function Header() {
           </div>
         </div>
         <Link href={"/help"}>
-          <span className="tracking-widest flex gap-2 items-center text-[11px] cursor-pointer hover:text-slate-400">
+          <span className="tracking-widest flex gap-2 items-center cursor-pointer hover:text-slate-400">
             Help
           </span>
         </Link>
