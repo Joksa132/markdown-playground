28ba9b3856dc80e7db3cb130d23cc2a7156f2b93
Joksa132 <nikolajoksimovic419@gmail.com>
2023-09-25 14:11:30 +0200

    make responsive

--- components/MarkdownInput.tsx
+++ components/MarkdownInput.tsx
@@ -1,22 +1,32 @@
 import CodeMirror from "@uiw/react-codemirror";
 import { EditorView } from "@codemirror/view";
+import { AiFillEye } from "react-icons/ai"
 
 type Props = {
   markdown: string;
   setMarkdownInput: React.Dispatch<React.SetStateAction<string>>;
   isMain: boolean;
+  setShowPreview: React.Dispatch<React.SetStateAction<boolean>>;
+  showPreview: boolean;
 }
 
-export default function MarkdownInput({ markdown, setMarkdownInput, isMain }: Props) {
+export default function MarkdownInput({ markdown, setMarkdownInput, isMain, setShowPreview, showPreview }: Props) {
 
   const handleMarkdownChange = (markdown: string) => {
     setMarkdownInput(markdown)
   }
 
   return (
-    <div className="w-1/2 border-[1px] border-slate-200 flex flex-col">
-      <div className="px-4 py-3">
+    <div className={`w-1/2 max-[600px]:w-full border-[1px] border-slate-200 ${showPreview ? 'max-[600px]:hidden' : 'max-[600px]:flex max-[600px]:flex-col'}`}>
+      <div className="px-4 py-3 flex justify-between items-center">
         <h2 className="uppercase text-xs text-slate-400 tracking-wider">markdown</h2>
+        <span
+          className="hidden max-[600px]:flex items-center gap-1 cursor-pointer uppercase text-xs text-slate-400 tracking-wider"
+          onClick={() => setShowPreview(true)}
+        >
+          <AiFillEye size={22} />
+          preview
+        </span>
       </div>
       {
         isMain ?
--- components/MarkdownPreview.tsx
+++ components/MarkdownPreview.tsx
@@ -1,16 +1,26 @@
 import ReactMarkdown from "react-markdown";
 import remarkGfm from 'remark-gfm'
+import { AiFillEyeInvisible } from "react-icons/ai"
 
 type Props = {
   markdown: string;
+  setShowPreview: React.Dispatch<React.SetStateAction<boolean>>;
+  showPreview: boolean;
 }
 
-export default function MarkdownPreview({ markdown }: Props) {
+export default function MarkdownPreview({ markdown, setShowPreview, showPreview }: Props) {
 
   return (
-    <div className="w-1/2 border-[1px] border-slate-200 flex flex-col">
-      <div className="px-4 py-3">
+    <div className={`w-1/2 max-[600px]:w-full border-[1px] border-slate-200 ${showPreview ? 'max-[600px]:flex max-[600px]:flex-col' : 'max-[600px]:hidden'}`}>
+      <div className="px-4 py-3 flex justify-between items-center">
         <h2 className="uppercase text-xs text-slate-400 tracking-wider">preview</h2>
+        <span
+          className="hidden max-[600px]:flex items-center gap-1 cursor-pointer uppercase text-xs text-slate-400 tracking-wider"
+          onClick={() => setShowPreview(false)}
+        >
+          <AiFillEyeInvisible size={22} />
+          markdown
+        </span>
       </div>
       <div className="border-t-[1px] h-[calc(100vh-88px)] overflow-y-scroll p-2 break-words w-full">
         <ReactMarkdown remarkPlugins={[remarkGfm]} className="prose">
