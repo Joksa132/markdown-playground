a743a1d7ab8efab9ad4816acfa950d543d0c12d9
Joksa132 <nikolajoksimovic419@gmail.com>
2023-09-25 14:45:37 +0200

    remove useless class

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <div className="flex flex-col w-full h-screen">
       <Header />
-      <main className="flex max-[600px]:flex-col">
+      <main className="flex">
         <MarkdownInput
           markdown={markdownInput}
           setMarkdownInput={setMarkdownInput}
