4c643a375d3863e85123b87d3c5c2db9ab86df4b
Joksa132 <nikolajoksimovic419@gmail.com>
2023-09-25 12:06:05 +0200

    import context

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,17 @@
 import Header from "@/components/Header"
 import MarkdownInput from "@/components/MarkdownInput"
 import MarkdownPreview from "@/components/MarkdownPreview"
-import { useState } from "react"
+import { MarkdownContext } from "@/context/MarkdownContext"
+import { useContext } from "react"
 
 export default function Home() {
-  const [markdownInput, setMarkdownInput] = useState<string>("")
+  const { markdownInput, setMarkdownInput } = useContext(MarkdownContext)
 
   return (
     <div className="flex flex-col w-full h-screen">
       <Header />
       <main className="flex">
-        <MarkdownInput setMarkdownInput={setMarkdownInput} />
+        <MarkdownInput markdown={markdownInput} setMarkdownInput={setMarkdownInput} />
         <MarkdownPreview markdown={markdownInput} />
       </main>
     </div>
