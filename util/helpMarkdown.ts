7518682ca2e9e0043d4a62175a40eae4578dbcf1
Joksa132 <nikolajoksimovic419@gmail.com>
2023-09-25 13:37:52 +0200

    create variable that stores markdown help

---
+++ util/helpMarkdown.ts
@@ -0,0 +1,46 @@
+export const helpMarkdown = `# Heading1 
+
+## Heading2
+
+### Heading3
+
+#### Heading4
+
+*italic text* or _italic text_
+
+**bold text** or __bold text__
+
+~strikethrough~ or ~~strikethrough~~
+
+[Link](https://markdown-playground-joksa132.vercel.app/)
+
+![Image](https://fastly.picsum.photos/id/676/536/354.jpg?hmac=AUe7ybcPXLBm37YhZd49jpbOql4HORAAWX-WUPSOhjg)
+
+> Quote
+
+* Unordered
+* List
+
+1. Ordered
+2. List
+
+A footnote[^1]
+
+\`\`\`
+code
+block
+\`\`\`
+
+| a | b | c |
+| - | - | - |
+| d | e | f |
+
+| a | b |
+| :- | -: |
+
+* [ ] to do
+* [x] done
+
+
+[^1]: a footnote.
+`
