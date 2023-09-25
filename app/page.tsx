0f04cb538806b5270f832feb5a7890fa1aea4f4c
Joksa132 <nikolajoksimovic419@gmail.com>
2023-09-25 14:11:14 +0200

    add state to track active component responsive

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,29 @@ import Header from "@/components/Header"
 import MarkdownInput from "@/components/MarkdownInput"
 import MarkdownPreview from "@/components/MarkdownPreview"
 import { MarkdownContext } from "@/context/MarkdownContext"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { SnackbarProvider } from 'notistack'
 
 export default function Home() {
   const { markdownInput, setMarkdownInput } = useContext(MarkdownContext)
+  const [showPreview, setShowPreview] = useState<boolean>(false)
 
   return (
     <div className="flex flex-col w-full h-screen">
       <Header />
-      <main className="flex">
-        <MarkdownInput markdown={markdownInput} setMarkdownInput={setMarkdownInput} isMain={true} />
-        <MarkdownPreview markdown={markdownInput} />
+      <main className="flex max-[600px]:flex-col">
+        <MarkdownInput
+          markdown={markdownInput}
+          setMarkdownInput={setMarkdownInput}
+          isMain={true}
+          setShowPreview={setShowPreview}
+          showPreview={showPreview}
+        />
+        <MarkdownPreview
+          markdown={markdownInput}
+          setShowPreview={setShowPreview}
+          showPreview={showPreview}
+        />
       </main>
       <SnackbarProvider autoHideDuration={3000} />
     </div>
