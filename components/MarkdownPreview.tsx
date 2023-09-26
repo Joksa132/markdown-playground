2c274b6b2f609507cb139a08608912f7aae41b91
Joksa132 <nikolajoksimovic419@gmail.com>
2023-09-26 16:47:02 +0200

    add useEffect to convert markdown to html

--- components/MarkdownPreview.tsx
+++ components/MarkdownPreview.tsx
@@ -1,14 +1,26 @@
+"use client"
+
 import ReactMarkdown from "react-markdown";
 import remarkGfm from 'remark-gfm'
 import { AiFillEyeInvisible } from "react-icons/ai"
+import { useEffect, useRef } from "react"
 
 type Props = {
   markdown: string;
   setShowPreview: React.Dispatch<React.SetStateAction<boolean>>;
   showPreview: boolean;
+  setConvertedHtml: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export default function MarkdownPreview({ markdown, setShowPreview, showPreview }: Props) {
+export default function MarkdownPreview({ markdown, setShowPreview, showPreview, setConvertedHtml }: Props) {
+  const markdownRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (markdownRef.current) {
+      const markdownHtml = markdownRef.current.innerHTML;
+      setConvertedHtml(markdownHtml)
+    }
+  }, [markdown])
 
   return (
     <div className={`w-1/2 max-[600px]:w-full border-[1px] border-slate-200 ${showPreview ? 'max-[600px]:flex max-[600px]:flex-col' : 'max-[600px]:hidden'}`}>
@@ -22,8 +34,8 @@ export default function MarkdownPreview({ markdown, setShowPreview, showPreview 
           markdown
         </span>
       </div>
-      <div className="border-t-[1px] h-[calc(100vh-90px)] overflow-y-scroll p-2 break-words w-full">
-        <ReactMarkdown remarkPlugins={[remarkGfm]} className="prose">
+      <div className="border-t-[1px] h-[calc(100vh-90px)] overflow-y-scroll p-2 break-words w-full prose" ref={markdownRef}>
+        <ReactMarkdown remarkPlugins={[remarkGfm]}>
           {markdown}
         </ReactMarkdown>
       </div>
