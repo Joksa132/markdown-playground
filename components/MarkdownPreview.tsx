f37f09a2cd965f2d8fd4c66ef7bc2b2992d339fd
Joksa132 <nikolajoksimovic419@gmail.com>
2023-09-26 18:24:33 +0200

    fix preview not going full width

--- components/MarkdownPreview.tsx
+++ components/MarkdownPreview.tsx
@@ -34,10 +34,12 @@ export default function MarkdownPreview({ markdown, setShowPreview, showPreview,
           markdown
         </span>
       </div>
-      <div className="border-t-[1px] h-[calc(100vh-90px)] overflow-y-scroll p-2 break-words w-full prose" ref={markdownRef}>
-        <ReactMarkdown remarkPlugins={[remarkGfm]}>
-          {markdown}
-        </ReactMarkdown>
+      <div className="border-t-[1px] h-[calc(100vh-90px)] overflow-y-scroll p-2 break-words w-full">
+        <div className="prose" ref={markdownRef}>
+          <ReactMarkdown remarkPlugins={[remarkGfm]}>
+            {markdown}
+          </ReactMarkdown>
+        </div>
       </div>
     </div>
   )
